c7559df0a8e07820c067a42f143e9f68ff433130
erginkerim2838 <127621738+erginkerim2838@users.noreply.github.com>
2024-02-16 16:03:50 +0300

    Add files via upload

---
+++ SplashScreen.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Levania_Launcher
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.loader = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 12;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockForm = false;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(226, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Night Launcher";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Location = new System.Drawing.Point(-6, 317);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(645, 24);
+            this.guna2Panel1.TabIndex = 2;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(35)))));
+            this.guna2Panel2.Location = new System.Drawing.Point(-7, 0);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(35, 24);
+            this.guna2Panel2.TabIndex = 3;
+            // 
+            // loader
+            // 
+            this.loader.Enabled = true;
+            this.loader.Interval = 50;
+            this.loader.Tick += new System.EventHandler(this.loader_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Version Kontrol Ediliyor..";
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(9)))), ((int)(((byte)(8)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(618, 337);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yükleniyor |Night Launcher";
+            this.Load += new System.EventHandler(this.SplashScreen_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Timer loader;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+    }
+}
+
---
+++ SplashScreen.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace Levania_Launcher
+{
+    public partial class SplashScreen : Form
+    {
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+        private string githubUrl = "https://raw.githubusercontent.com/erginkerim2838/night-launcher/main/version";
+        private string version = "1.0.0";
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.DiscordRPC)
+            {
+                RPC();
+            }
+            loader.Start();
+        }
+
+        private DiscordRPC.EventHandlers handlers;
+        private DiscordRPC.RichPresence presence;
+        void RPC()
+        {
+            this.handlers = default(DiscordRPC.EventHandlers);
+            DiscordRPC.Initialize("1207339306713223259", ref this.handlers, true, null);
+            this.presence.details = "Night Launcher";
+            this.presence.state = "Yukleniyor Ekraninda.";
+            this.presence.largeImageKey = "logo";
+            this.presence.largeImageText = "Night Launcher !";
+            this.presence.startTimestamp = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
+            DiscordRPC.UpdatePresence(ref this.presence);
+        }
+
+        private void loader_Tick(object sender, EventArgs e)
+        {
+            guna2Panel2.Width += 5;
+            if (guna2Panel2.Width >= 0)
+            {
+                CheckVersion();
+                label2.Text = "Version Kontrol Ediliyor..";
+            }
+            else if (guna2Panel2.Width >= 150)
+            {
+                label2.Text = "Version Kontrol Edildi.";
+
+            }
+            if (guna2Panel2.Width >= 300)
+            {
+                label2.Text = "İnternet Bağlantısı Kontrol Ediliyor...";
+            }
+            if (guna2Panel2.Width >= 400)
+            {
+                label2.Text = "İnternet Bağlantısı Kontrol Edildi.";
+
+
+            }
+            if (guna2Panel2.Width >= 560)
+            {
+                label2.Text = "Kontroller Başarılı. Başlatılıyor...";
+            }
+            if (guna2Panel2.Width >= guna2Panel1.Width)
+            {
+                LoginScreen ls = new LoginScreen();
+                ls.Show();
+                Hide();
+                loader.Stop();
+            }
+
+
+        }
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void CheckVersion()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string remoteVersion = client.DownloadString(githubUrl).Trim();
+
+                    if (remoteVersion != version)
+                    {
+                        loader.Stop();
+                        MessageBox.Show("Yeni Güncelleme Mevcut !");
+                        Process.Start("https://github.com/Fr3zyy/LevaniaLauncher");
+                        Application.Exit();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                loader.Stop();
+                MessageBox.Show("İnternet Bağlantısı Bulunamadı !");
+                Application.Exit();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
